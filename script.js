abc396b4cc52197ec9bfa99d82c7ba6bdf3db126
Lanahv <126159032+Lanahv@users.noreply.github.com>
2023-04-27 09:06:33 -0300

    .

--- script.js
+++ script.js
@@ -5,10 +5,3 @@ Clique aqui para aparecer a hora atual</button>
 }
 <p id="demo"></p>
 
-div.fixed {
-    position: fixed;
-    bottom: 0;
-    right: 20;
-    width: 300px;
-    border: 3px solid #73AD21;
-  }

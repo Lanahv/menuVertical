c5fe3a8c98f34e251b229e02c9bdf17d58b38dfb
Lanahv <126159032+Lanahv@users.noreply.github.com>
2023-08-18 09:49:17 -0300

    up

--- script.js
+++ script.js
@@ -1,2 +1,7 @@
-
+{
+    <button type="button"
+    onclick="document.getElementById('demo').innerHTML = Date()">
+    Clique aqui para aparecer a hora atual</button>
+    }
+    <p id="demo"></p>
 
